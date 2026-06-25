a71d7c922a48d52db4c9e9185dd1468bab67b185
Sathwik-Samudrala <sathwik.samudrala915@gmail.com>
2026-06-25 11:41:17 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=CORS_ORIGINS,
-    allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
+    allow_origin_regex=r"https://fashion-ai-frontend-osvx.onrender.com",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
